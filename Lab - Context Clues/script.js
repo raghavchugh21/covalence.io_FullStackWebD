0d7ab91696bebd8b9ec3368fc3f83aa755285704
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2020-06-09 00:44:04 +0530

    Updated Context Clues script.js

--- Lab - Context Clues/script.js
+++ Lab - Context Clues/script.js
@@ -1,16 +1,17 @@
 $("document").ready(function(){
-    for(var i =1;i<=100;i++)
-    {
-        $("<h3 id='"+i.toString()+"'>Accusation "+i.toString()+"</h3>").appendTo("body");
-    }
     var friends= ["Sarthak","Aryan","Taman","Anirudh","Chaitanya"];
     var locations = ["Garden","Pool","Kitchen","Dining Room","Drawing Room","Bathroom","Bedroom","Study Room","Balcony","Lawn"];
-    var weapons = ['A-bomb','ammo','ammunition','armaments','arms','arrow','assault rifle','atom bomb','atomic bomb', 'cannon','automatic rifle','axe','ballista','ballistic missile','bat','baton','battle axe',"bayonet","gun","knife"]
+    var weapons = ['A-bomb','ammo','ammunition','armaments','arms','arrow','assault rifle','atom bomb','atomic bomb', 'cannon','automatic rifle','axe','ballista','ballistic missile','bat','baton','battle axe',"bayonet","gun","knife"];
 
-    for(var i =1;i<=100;i++)
-    $("#"+i.toString()).click(function accuseAlert(){
-        alert("Accusation "+this.id+" : I accuse "+friends[this.id%5]+", with the "+ weapons[this.id%20]+" in the "+locations[this.id%10]+"!");
-    });
-    
+    function accuseAlert(x){
+        alert("Accusation "+x+" : I accuse "+friends[x%5]+", with the "+ weapons[x%20]+" in the "+locations[x%10]+"!");
+    };
+
+    for(var i =1;i<=10;i++)
+    {
+        $("<h3 id='"+i.toString()+"'>Accusation "+i.toString()+"</h3>").appendTo("body");
+        document.getElementById(i.toString()).addEventListener("click",accuseAlert.bind(this, i-1));
+    }
+
  
 });
