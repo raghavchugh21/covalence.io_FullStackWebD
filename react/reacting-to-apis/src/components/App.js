e821f032c78a94bc3ab278b0661c5df483550d3d
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2020-06-23 02:01:35 +0530

    reacting-to-apis Phase Two update

--- react/reacting-to-apis/src/components/App.js
+++ react/reacting-to-apis/src/components/App.js
@@ -5,25 +5,38 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            films: []
+            films: [],
+            filmList: [],
+            i: 0,
+            created: true
         }
+        this.addFilm = this.addFilm.bind(this);
     }
-
 
     componentDidMount() {
         fetch('https://ghibliapi.herokuapp.com/films')
             .then(res => res.json())
-            .then(films => this.setState({ films }))
+            .then(films => this.setState({ films }));
+    }
 
+    addFilm() {
+        this.setState({created:false})
+        this.setState({ filmList: [...this.state.filmList, this.state.films[this.state.i++]] });
+        console.log(this.state.films[0]);
     }
 
     render() {
-        return (<Fragment>
-            {this.state.films.map((film, index) => {
-                return (<Card title={film.title}
+        if(this.state.created){
+        this.state.films = this.state.films.map((film) => {
+            return (
+                <Card title={film.title}
                     desc={film.description}
-                    director={film.director} />);
-            })}
+                    director={film.director} />
+            );
+        })}
+        return (<Fragment>
+            {this.state.filmList}
+            <button className="btn btn-outline-secondary" type="button" onClick={this.addFilm} >Post</button>
         </Fragment>);
     }
 
