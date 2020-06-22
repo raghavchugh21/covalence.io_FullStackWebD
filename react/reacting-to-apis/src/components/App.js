e79785256579160cea2ce36b213106a7247cea4e
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2020-06-23 02:39:03 +0530

    refining reacting-to-apis Phase Two

--- react/reacting-to-apis/src/components/App.js
+++ react/reacting-to-apis/src/components/App.js
@@ -5,40 +5,33 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            films: [],
-            filmList: [],
-            i: 0,
-            created: true
+            films : [],
+            i : 0,
+            film : []
         }
         this.addFilm = this.addFilm.bind(this);
     }
 
     componentDidMount() {
-        fetch('https://ghibliapi.herokuapp.com/films')
+        fetch('https://ghibliapi.herokuapp.com/films/')
             .then(res => res.json())
-            .then(films => this.setState({ films }));
+            .then(films => this.setState({films}));
     }
 
-    addFilm() {
-        this.setState({created:false})
-        this.setState({ filmList: [...this.state.filmList, this.state.films[this.state.i++]] });
-        console.log(this.state.films[0]);
+    addFilm(){
+        fetch(`https://ghibliapi.herokuapp.com/films/${this.state.films[this.state.i++].id}`)
+        .then(res => res.json())
+        .then(film => this.setState({film : [...this.state.film, <Card title={film.title} desc={film.description} director={film.director} />]}));
+
     }
 
-    render() {
-        if(this.state.created){
-        this.state.films = this.state.films.map((film) => {
-            return (
-                <Card title={film.title}
-                    desc={film.description}
-                    director={film.director} />
-            );
-        })}
+     render() {
+ 
         return (<Fragment>
-            {this.state.filmList}
-            <button className="btn btn-outline-secondary" type="button" onClick={this.addFilm} >Post</button>
-        </Fragment>);
-    }
+             {this.state.film}
+             <button className="btn btn-outline-secondary" type="button" onClick={this.addFilm} >Post</button>
+         </Fragment>);
+     }
 
 }
 
